eceb58b64de0bc724b3ba3c81912229d85218273
Magomed Elmurzaev <ban0le4@gmail.com>
2024-08-21 19:41:51 +0300

    created header

---
+++ components/ui/index.ts
@@ -0,0 +1,10 @@
+export { Button } from './button';
+export { Checkbox } from './checkbox';
+export { Dialog } from './dialog';
+export { Input } from './input';
+export { Skeleton } from './skeleton';
+export { Slider } from './slider';
+export { Popover } from './popover';
+export { Drawer } from './drawer';
+export { Select } from './select';
+
